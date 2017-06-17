c4c4bef682d54c3357279923b084d8c884b91ba1
xy <xy@xydemac.local>
2017-06-17 16:22:36 +0800

    add backgroundColor

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     NSLog(@"111");
+    
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 - (void)didReceiveMemoryWarning {
